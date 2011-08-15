24389d44ba4af939386a14db6fd7eb512c64a43b
T S <YenTheFirst@gmail.com>
2011-08-15 16:57:33 -0700

    add fixtures & test for recursive folder search

--- spec/arcgis/dowloader_spec.rb
+++ spec/arcgis/dowloader_spec.rb
@@ -15,4 +15,20 @@ describe ArcGIS::Downloader do
     end
   end
 
+  describe "each_service" do
+    it "should fetch all service descriptors, recursively" do
+      stub_json_get("").to_return(:status => 200, :body => fixture("small_index.json"))
+      stub_json_get("folder1").to_return(:status => 200, :body => fixture("folder1.json"))
+      stub_json_get("folder2").to_return(:status => 200, :body => fixture("folder2.json"))
+
+      all_services = @downloader.each_service.to_a
+      ["","folder1","folder2"].each {|p| a_json_get(p).should have_been_made}
+
+      all_services.length.should == 4 #there are 4 services in total. 1 in index, 1 in folder1, 2 in folder2
+      all_services.select {|path,name,type| path.empty?}.length.should == 1 #there should be 1 service with an empty path - the one from the index
+    end
+
+    #TODO: need to find real-world example of sub-folder setup, test it.
+  end
+
 end
